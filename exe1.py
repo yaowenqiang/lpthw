8729dfacde07b585b5e95405bb13ac4ebed4841f
vagrant <vagrant@homestead>
2018-10-13 19:46:37 +0000

    init commit

---
+++ exe1.py
@@ -0,0 +1,7 @@
+print('hello world!')
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd mucher rather you 'not'.")
+print('I "said" do not touch this.')
