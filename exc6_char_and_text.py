9ece572924e1f102060f33a2c48d50a6cbcab421
yaowenqiang <yaowenqiang111@163.com>
2018-10-20 02:27:43 +0000

    add exc6_char_and_text

---
+++ exc6_char_and_text.py
@@ -0,0 +1,26 @@
+types_of_people = 20
+x = f"There are {types_of_people} types of people"
+
+binary = "binary"
+
+do_not = "don't"
+
+y = f"Those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"i said: {x}")
+
+print(f"i also said: {y}")
+
+hilarious = "False" # 滑稽
+
+joke_evaluation = "Isn't that joke so funny!"
+
+print(joke_evaluation. format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w + e)
+
