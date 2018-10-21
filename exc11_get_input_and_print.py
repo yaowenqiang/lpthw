98df6a028629c94523cf8c4d415544b09882b37b
yaowenqiang <yaowenqiang111@163.com>
2018-10-21 03:24:38 +0000

    add exc11_get_input_and_print

---
+++ exc11_get_input_and_print.py
@@ -0,0 +1,9 @@
+print("How old are you",end=" ")
+age = input()
+print("How tall are you",end=" ")
+height = int(input())
+print("How much are you weight",end=" ")
+weight = input()
+
+print(f"So,you're {age} old,{height} tall and {weight} heavy.")
+
