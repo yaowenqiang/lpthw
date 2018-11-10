562748d03d097aec3bfe1f21fa38a1a66d568bff
yaowenqiang <yaowenqiang111@163.com>
2018-11-10 16:05:10 +0000

    add self argument in the __init__ method

--- exc42_super.py
+++ exc42_super.py
@@ -1,13 +1,13 @@
 class A:
-    def __init__():
+    def __init__(self):
         print("i am class A")
+        pass
 
 
 class B(A):
-    def __init():
+    def __init__(self):
         super().__init__()
         print("i am class B")
 
 b = B()
-b.__init()
 
