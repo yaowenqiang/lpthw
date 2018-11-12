11639a752864cf33d2911250a0c35bbc7578345a
yaowenqiang <yaowenqiang111@163.com>
2018-11-12 19:08:11 +0000

    add some proto code

--- exc43_classes.py
+++ exc43_classes.py
@@ -1,13 +1,26 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
 class Scene(object):
     def enter(self):
-        pass
+        print('This scene is not yet configured.')
+        print('Subclass it and implement enter()')
+        exit(1)
 
 class Engine(object):
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
     
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+    
+        current_scene.enter()
 
 class Death(Scene):
     def __enter(self):
