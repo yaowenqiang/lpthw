e63e69f2cb4813e0d60f3880d49e8b6d5a6fde2b
yaowenqiang <yaowenqiang111@163.com>
2018-11-21 17:15:54 +0000

    add more oop code

--- exc44_inheritance_and_combination.py
+++ exc44_inheritance_and_combination.py
@@ -6,7 +6,7 @@ class Parent:
 class Child(Parent):
     def altered(self):
         print("CHILD before PARENT altered!")
-        super(Child,self).altered()
+        super(Child, self).altered()
         print("CHILD after PARENT altered!")
 
 dad = Parent()
@@ -14,3 +14,34 @@ sun = Child()
 
 dad.altered()
 sun.altered()
+
+
+class Other():
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child():
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        self.other.override()
+
+    def altered(self):
+        print("CHILD,BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD,AFTER OTHER altered()")
+
+son1 = Child()
+son1.override()
+son1.implicit()
+son1.altered()
