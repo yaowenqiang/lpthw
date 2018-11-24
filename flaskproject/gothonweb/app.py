18cf5e249383454a2d60f8cc346b1e7dadbee3f8
yaowenqiang <yaowenqiang111@163.com>
2018-11-24 15:18:50 +0000

    add html template and form

--- flaskproject/gothonweb/app.py
+++ flaskproject/gothonweb/app.py
@@ -1,9 +1,33 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return "Hello World"
+    greeting = "Hello World"
+    name = request.args.get('name','nobody')
+    if name:
+        greeting = f"Hello  {name}"
+    else:
+        greeting = "Hello  world"
+
+    return render_template("index.html",greeting=greeting)
+
+@app.route('/hello' ,methods=["GET","POST"])
+
+def index():
+    greeting = "Hello World"
+    if request.method == "POST":
+        name = request.form["name"]
+        greet = request.form["greet"]
+
+        greeting = f"{greet} {name}"
+
+        return render_template("index.html",greeting=greeting)
+    else:
+
+        return render_template("form.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
