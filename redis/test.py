33610b5027a0aa847e6b2e63c2610b10b4966256
yaowenqiang <yaowenqiang111@163.com>
2018-11-28 17:56:30 +0000

    add get_articles function

--- redis/test.py
+++ redis/test.py
@@ -1,12 +1,13 @@
 from time import time
 from redis import *
 ONE_WEEK_IN_SECONDS = 7 * 867400
+ARTICLES_PER_PAGE = 15
 VOTE_SCORE = 432
 
 
 def article_vote(conn, user, article):
     cutoff = time() - ONE_WEEK_IN_SECONDS
-    if conn.zscore("time:", article) < cutoff:
+    if conn.zscore("time:", article) == None or conn.zscore("time:", article) < cutf8 :
         return
 
     article_id = article.partition(":")[-1]
@@ -39,9 +40,31 @@ def post_article(conn, user, title, link):
     return article_id
 
 
+def get_article(conn, page, order="score:"):
+    start = (page - 1) * ARTICLES_PER_PAGE
+    end = start + ARTICLES_PER_PAGE - 1
+
+    ids = conn.zrevrange(order, start, end)
+
+    articles = []
+
+    for id in ids:
+        article_data = conn.hgetall(id)
+        article_data['id'] = id
+        articles.append(article_data)
+
+    return articles
+
+
+
 redis = Redis()
 
 user = 1
 article = 1
 post_article(redis, user, 'title', 'link')
-# article_vote(redis, user, article)
+article_vote(redis, user, article)
+articles = get_article(redis, 1)
+for article in articles:
+    print(article)
+
+
