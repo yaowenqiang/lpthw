319fd7356ce2e09ff3ed327315481a84abf9eb26
yaowenqiang <yaowenqiang111@163.com>
2018-12-05 17:59:29 +0000

    add test

---
+++ oop/MyInteger.py
@@ -0,0 +1,21 @@
+class MyInteger(object):
+    def set_val(self, val):
+        try:
+            val - int(val)
+        except ValueError:
+            return
+        
+        self.val = val
+
+    def get_val(self):
+        return self.val
+    
+    def increment_val(self):
+        self.val = self.val + 1
+
+i = MyInteger()
+i.set_val(9)
+print(i.get_val())
+
+i.set_val('hi')
+print(i.get_val())
