e59f76307b7c19f0c47c6cac1b22ae67ba8fb079
yaowenqiang <yaowenqiang111@163.com>
2018-12-13 18:19:24 +0000

    add abstract_class demo

---
+++ oop/abstract_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import abc
+
+class GetterSetter(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def set_val(self, input):
+        """Set a value in the instance."""
+        return
+
+    @abc.abstractmethod
+    def get_val(self):
+        """Get a value in the instance."""
+        return
+
+
+class MyClass(GetterSetter):
+    def set_val(self,input):
+        self.val = input
+
+    #def get_val(self):
+    #    'return self.val'
+
+x = MyClass()
+
+print(x)
