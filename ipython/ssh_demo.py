35c4424f6e700e5eab7e8a5734b0348c777ee100
yaowenqiang <yaowenqiang111@163.com>
2018-12-26 16:21:11 +0000

    fix ssh error

--- ipython/ssh_demo.py
+++ ipython/ssh_demo.py
@@ -1,19 +1,26 @@
 import paramiko
+import pprint
 
 hostname = 'localhost'
 port = '22'
 
-username = 'root'
-password = '1223456'
+username = 'vagrant'
+password = 'vagrant'
 
 if __name__ == '__main__':
     paramiko.util.log_to_file("paramkio.log")
 
     s = paramiko.SSHClient()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     s.load_system_host_keys()
     s.connect(hostname, port, username, password)
     stdin, stdout, stderr = s.exec_command('ifconfig')
+    print((stdout.read().decode('utf-8')))
 
-    print(stdout.read())
+    stdin, stdout, stderr = s.exec_command('hostname')
+    print((stdout.read().decode('utf-8')))
+
+    stdin, stdout, stderr = s.exec_command('who')
+    print((stdout.read().decode('utf-8')))
 
     s.close()
