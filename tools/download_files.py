7e98a6052621a7979ff115ead0c45676413b47d2
yaowenqiang <yaowenqiang111@163.com>
2018-12-26 03:33:47 +0000

    add beautifulsoup  demo

--- tools/download_files.py
+++ tools/download_files.py
@@ -1,5 +1,7 @@
 import requests
-response = requests.get("https://automatetheboringstuff.com/files/rj.txt")
+import bs4
+response = requests.get("https://www.baidu.com/")
+#  response = requests.get("https://automatetheboringstuff.com/files/rj.txt")
 print(response.status_code)
 try:
     response.raise_for_status()
@@ -9,8 +11,16 @@ if response.status_code == requests.codes.ok:
     print("ok")
 print(len(response.text))
 print(response.text[:50])
-play_file = open("save.txt", 'wb')
-for chunk in response.iter_content(5000):
-    play_file.write(chunk)
 
-play_file.close()
+soup = bs4.BeautifulSoup(response.text, features='html.parser')
+#  print(type(soup))
+#  play_file = open("save.txt", 'wb')
+#  for chunk in response.iter_content(5000):
+    #  play_file.write(chunk)
+
+#  play_file.close()
+
+elems = soup.select('body')
+#  print(type(elems[0].getText()))
+print(elems[0].getText())
+print(elems[0].attrs)
