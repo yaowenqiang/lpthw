f3929ec423f02b9315f41439d7d734488eb8cc12
yaowenqiang <yaowenqiang111@163.com>
2019-01-01 23:13:22 +0800

    update pytest demo add setup_class and teardown_class demo

--- oop/myprogram.py
+++ oop/myprogram.py
@@ -2,12 +2,20 @@ import sys
 def doubleit(x):
     if not  isinstance(x, (int, float)):
         raise TypeError
-        
     var = x * 2
     return var
 
+def doublelines(filename):
+    with open(filename, 'r') as fh:
+        newlist = [str(doubleit(int(val))) for val in fh]
+    
+    with open(filename, 'w') as fh:
+        fh.write("\n".join(newlist))
+
 if __name__ == '__main__':
-    input_val = sys.argv[1]
+    input_val = int(sys.argv[1])
     doubled_val = doubleit(input_val)
+
+    doublelines('testnums_template.txt')
 
     print(f"The value of {input_val} is {doubled_val}")
--- oop/test_myprogram.py
+++ oop/test_myprogram.py
@@ -1,10 +1,37 @@
 import myprogram
+import shutil
 import pytest
+import os
 
-def test_doubleit():
-    assert myprogram.doubleit(10) == 20
+class TestDoubleit(object):
+    
+    numbers_file_template = 'testnums_template.txt'
+    numbers_file_testor = 'testnums.txt'
+
+    def setup_class(self):
+        shutil.copy(TestDoubleit.numbers_file_template, TestDoubleit.numbers_file_testor)
+
+    
+    def teardown_class(self):
+        os.remove(TestDoubleit.numbers_file_testor)
+
+    
+    def test_doublelines(self):
+        myprogram.doublelines(TestDoubleit.numbers_file_testor)
+        old_vals = [int(line) for line in open(TestDoubleit.numbers_file_template)]
+        new_vals = [int(line) for line in open(TestDoubleit.numbers_file_testor)]
+
+        for old_val, new_val in zip(old_vals, new_vals) :
+            assert int(new_val) == int(old_val) * 2
 
 
-def test_doubleit_type():
-    with pytest.raises(TypeError):
-        myprogram.doubleit('hello')
+    def test_doubleit_value(self):
+        assert myprogram.doubleit(10) == 20
+
+    def test_doubleit(self):
+        assert myprogram.doubleit(10) == 20
+
+
+    def test_doubleit_type(self):
+        with pytest.raises(TypeError):
+            myprogram.doubleit('hello')
