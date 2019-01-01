b8902af7a2f52d38bab5b8481237ec6d5f7c2e94
yaowenqiang <yaowenqiang111@163.com>
2019-01-01 20:18:14 +0800

    add multiprocessing_queue_demo

---
+++ concurrency/multiprocessing_queue_demo.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process
+from multiprocessing import Queue
+import random
+def make_tuple(queue):
+    num = random.randint(1,9)
+    queue.put(("Hi", num))
+    print(queue.get())
+
+def make_string(queue):
+    tup = queue.get()
+    result = ''
+    substr, num = tup
+    
+    for _ in range(num):
+        result += substr
+    
+    queue.put(result)
+
+if __name__ == '__main__':
+    queue = Queue()
+    
+    p1 = Process(target=make_tuple, args=(queue,))
+    p2 = Process(target=make_string, args=(queue,))
+    
+    p1.start()
+    p2.start()
