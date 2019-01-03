d99d3b206a394a258bdf7a68982a174b26dff412
yaowenqiang <yaowenqiang111@163.com>
2019-01-03 04:36:38 +0000

    add 周报相关代码

--- excel/xlsxwriter_demo.py
+++ excel/xlsxwriter_demo.py
@@ -1,9 +1,14 @@
+# coding=utf-8
+
 import xlsxwriter as xw
+from time import strftime, gmtime
+timestr = strftime("-%d-%m-%Y",gmtime())
 
-workbook = xw.Workbook("mybook.xlsx")
+file_name = "周报-姚文强" + timestr + ".xlsx"
+workbook = xw.Workbook(file_name)
 
-WORKSHEEt = workbook.add_worksheet()
+worksheet = workbook.add_worksheet()
 
-worksheet.write("A1",1)
+worksheet.write("A1","你好")
 
 workbook.close()
