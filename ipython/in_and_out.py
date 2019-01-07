735f9a8133a89f812fbce967e59361afa9dbeb75
yaowenqiang <yaowenqiang111@163.com>
2019-01-08 02:54:01 +0800

    add ipython demo

---
+++ ipython/in_and_out.py
@@ -0,0 +1,12 @@
+class Demo():
+    def __str__(self):
+        return '__str__'
+
+    def __repr__(self):
+        return '__repr__'
+
+
+d = Demo()
+
+print(d)
+# https://stackoverflow.com/questions/1436703/difference-between-str-and-repr
