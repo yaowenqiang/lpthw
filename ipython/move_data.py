ed461c22e8b4fdebb6f349fdae695d046a20c98f
yaowenqiang <yaowenqiang111@163.com>
2019-01-20 23:47:57 +0800

    add move_data demo of the shutil module

---
+++ ipython/move_data.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+from pprint import pprint
+print(os.getcwd())
+
+# os.mkdir('test')
+pprint(os.listdir())
+os.rmdir('tet')
+
+pprint(os.listdir('sys_tool'))
+
+# shutil.copytree(src, dst)
+# shutil.rmtree()
