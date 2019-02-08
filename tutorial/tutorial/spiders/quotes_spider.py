48493c7d5739b4c8f512d4af211ccd7e960d8042
yaowenqiang <yaowenqiang111@163.com>
2019-02-08 15:36:51 +0800

    add scrapy follow link demo

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -2,14 +2,22 @@ import scrapy
 class QoutesSpider(scrapy.Spider):
     name = "qoutes"
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-        ]
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
 
 
     def parse(self, response):
         for quote in response.css('div.quote'):
+            self.log('parse')
             yield {
-                'text': quote.css('span.text::text').get()
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
             }
+            self.log('parse')
+
+        #  next_page = response.css('li.next a::attr(href)').get()
+        #  if next_page is not None:
+            #  next_page = response.urljoin(next_page)
+            #  yield scrapy.Request(next_page, callback=self.parse)
