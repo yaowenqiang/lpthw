14271994c2884ceb6b90a78a57f244b43671131a
yaowenqiang <yaowenqiang111@163.com>
2019-02-11 23:37:31 +0800

    add recursive option to the pyinotify demo

--- ipython/pyinotify_demo_p3.py
+++ ipython/pyinotify_demo_p3.py
@@ -2,5 +2,5 @@ import pyinotify
 wm = pyinotify.WatchManager()
 notifier = pyinotify.Notifier(wm)
 
-wm.add_watch('/tmp/', pyinotify.ALL_EVENTS)
+wm.add_watch('/tmp/', pyinotify.ALL_EVENTS, rec=True)
 notifier.loop()
