7ea348ac9058c096736f2583c13716d98bbc51f1
yaowenqiang <yaowenqiang111@163.com>
2019-03-13 01:04:02 +0800

    add redis info demo

---
+++ redis/redis_info.py
@@ -0,0 +1,20 @@
+import redis
+
+client = redis.StrictRedis()
+
+print(client.info('stats')['instantaneous_ops_per_sec'])
+
+print(client.info('clients')['connected_clients'])
+
+print(client.execute_command('client list'))
+
+print(client.info('stats')['rejected_connections'])
+
+print(client.info('memory')['used_memory_human'])
+
+print(client.info('memory')['used_memory_rss_human'])
+
+print(client.info('memory')['used_memory_peak_human'])
+
+print(client.info('memory')['used_memory_lua_human'])
+
